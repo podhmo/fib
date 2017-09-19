ff5b128af81dc26c399e0ce25c77ef080cc57821
podhmo <ababjam61+github@gmail.com>
2017-09-20 08:07:50 +0900

    modified more gently

--- examples/run2.py
+++ examples/run2.py
@@ -2,7 +2,5 @@ import ctypes
 import os.path
 
 fib = ctypes.CDLL(os.path.join(os.path.dirname(__file__), "../dist/libfib.so"))
-fib.fib.argtypes = [ctypes.c_int]
-fib.fib.restype = ctypes.c_int
-L = range(11)
-print([fib.fib(x) for x in L])
+# stack overflow
+fib.fib("foo")
